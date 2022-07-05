605d67477cd8f539d15707f0cc0dd89b89669a7c
Kevin Chandra <kacol.bot@gmail.com>
2022-07-05 13:36:13 +0700

    remove spaces

--- resources/views/brand/wa-order-list.blade.php
+++ resources/views/brand/wa-order-list.blade.php
@@ -1,11 +1,8 @@
 @foreach($orderItems as $product)
-{{ $product->name }}
-----------
 @foreach($product->orderItems as $item)
 {{ $item->quantity }}x {{ $product->name }}
 @if($item->notes)
     _(notes: {{ $item->notes }})_
 @endif
 @endforeach
-    
 @endforeach
