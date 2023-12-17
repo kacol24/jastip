e2a58bf1041f1e408ec78dd2414c9d65df45b8d1
Kevin Chandra <kacol.bot@gmail.com>
2023-12-17 15:13:56 +0700

    add sleep between cache

--- routes/console.php
+++ routes/console.php
@@ -20,22 +20,34 @@ Artisan::command('inspire', function () {
 
 Artisan::command('deploy', function () {
     $this->call('optimize');
+    sleep(1);
     $this->call('config:cache');
-    $this->call('event:cache');
+    sleep(1);
     $this->call('route:cache');
+    sleep(1);
     $this->call('view:cache');
+    sleep(1);
+    $this->call('event:cache');
+    sleep(1);
     $this->call('queue:restart');
+    sleep(1);
 
-    //$this->call('icons:cache');
+    $this->call('icons:cache');
 })->purpose('Cache and optimize. Used for production.');
 
 Artisan::command('deploy:clear', function () {
     $this->call('optimize:clear');
+    sleep(1);
     $this->call('config:clear');
-    $this->call('event:clear');
+    sleep(1);
     $this->call('route:clear');
+    sleep(1);
     $this->call('view:clear');
+    sleep(1);
+    $this->call('event:clear');
+    sleep(1);
     $this->call('queue:restart');
+    sleep(1);
 
-    //$this->call('icons:clear');
+    $this->call('icons:clear');
 })->purpose('Clears all cache and optimizations.');
